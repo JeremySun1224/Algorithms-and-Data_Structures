eaaccecaa9955b7603c9f0ad53b6c6ed266ce1c5
jeremysun1224 <2113751293@qq.com>
2021-03-21 14:50:44 +0800

    bucket sort

--- 11.bucket_sort.py
+++ 11.bucket_sort.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 # -*- author: JeremySun -*-
 # -*- dating: 21/3/21 -*-
+
+"""桶排序"""
+
+
+def bucket_sort(li, n=100, max_num=10000):
+    buckets = [[] for _ in range(n)]  # 创建桶
+    for val in li:
+        i = min(val // (max_num // n), n - 1)  # i表示val放到几号桶里
+        buckets[i].append(val)
+        for j in range(len(buckets[i]) - 1, 0, -1):
+            if buckets[i][j] < buckets[i][j-1]:
+                buckets[i][j], buckets[i][j-1] = buckets[i][j-1], buckets[i][j]
+            else:
+                break 
