c49078a69f5f7fe413a357fe2eb443771a171ae2
jeremysun1224 <2113751293@qq.com>
2021-03-22 10:40:36 +0800

    bucket sort

--- 11.bucket_sort.py
+++ 11.bucket_sort.py
@@ -4,14 +4,26 @@
 
 """桶排序"""
 
+import random
+
 
 def bucket_sort(li, n=100, max_num=10000):
     buckets = [[] for _ in range(n)]  # 创建桶
     for val in li:
         i = min(val // (max_num // n), n - 1)  # i表示val放到几号桶里
-        buckets[i].append(val)
-        for j in range(len(buckets[i]) - 1, 0, -1):
-            if buckets[i][j] < buckets[i][j-1]:
-                buckets[i][j], buckets[i][j-1] = buckets[i][j-1], buckets[i][j]
+        buckets[i].append(val)  # 把var加到桶里面
+        for j in range(len(buckets[i]) - 1, 0, -1):  # 用类似插入排序的方法保持桶内顺序
+            if buckets[i][j] < buckets[i][j - 1]:
+                buckets[i][j], buckets[i][j - 1] = buckets[i][j - 1], buckets[i][j]
             else:
-                break 
+                break
+    sorted_li = []
+    for buc in buckets:
+        sorted_li.extend(buc)
+    return sorted_li
+
+
+if __name__ == '__main__':
+    li = [random.randint(0, 10000) for _ in range(100000)]
+    # print(f'排序前: {li}')
+    print(f'排序后: {bucket_sort(li=li)}')
