c349e2e038f7732bf16222896436e8ad427ec9c8
jeremysun1224 <2113751293@qq.com>
2021-04-10 11:42:36 +0800

    cut rod recurrent

--- 26.cut_rod.py
+++ 26.cut_rod.py
@@ -32,11 +32,6 @@ def cut_rod_recurrent_1(p, n):
         return res
 
 
-@cal_time
-def c1(p, n):
-    return cut_rod_recurrent_1(p, n)
-
-
 def cut_rod_recurrent_2(p, n):
     """左边不切割，只切割右边"""
     if n == 0:
@@ -49,11 +44,16 @@ def cut_rod_recurrent_2(p, n):
 
 
 @cal_time
+def c1(p, n):
+    return cut_rod_recurrent_1(p, n)
+
+
+@cal_time
 def c2(p, n):
     return cut_rod_recurrent_2(p, n)
 
 
 if __name__ == '__main__':
     price = [0, 1, 5, 8, 9, 10, 17, 17, 20, 21, 23, 24, 26, 27, 27, 28, 30, 33, 36, 39, 40]
-    print(c1(p=price, n=20))
-    print(c2(p=price, n=20))
+    print(c1(p=price, n=16))
+    print(c2(p=price, n=16))
